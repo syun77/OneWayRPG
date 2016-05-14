ac3a83b6f13cfda03c53289755dbd375bdbf68ff
syun77 <2dgames.jp@gmail.com>
2016-05-14 20:29:59 +0900

    fixed #12 OneWayRPG-12 Globalの不要なコードを削除

--- source/jp_2dgames/game/global/Global.hx
+++ source/jp_2dgames/game/global/Global.hx
@@ -16,20 +16,12 @@ class Global {
   public static inline var MAX_LEVEL:Int = 6;
   public static inline var MAX_LIFE:Int = 100;
   static inline var START_LEVEL:Int = 1;
-  static inline var FIRST_SHOT:Float = 0.0;
-  static inline var MAX_SHOT:Float = 100.0;
   static inline var FIRST_MONEY:Int = 0;
 
-  // HP
-  static var _life:Float;
   // スコア
   static var _score:Int;
   // レベル
   static var _level:Int;
-  // ショットゲージ
-  static var _shot:Float;
-  // カギの所持数
-  static var _keys:Int;
   // お金
   static var _money:Int;
   // プレイヤーステータス
@@ -39,10 +31,8 @@ class Global {
   // ショップアイテム
   static var _shop:Array<ItemData>;
 
-  public static var life(get, never):Float;
   public static var score(get, never):Int;
   public static var level(get, never):Int;
-  public static var shot(get, never):Float;
   public static var money(get, never):Int;
   public static var step(get, never):Int;
   public static var shop(get, never):Array<ItemData>;
@@ -57,10 +47,8 @@ class Global {
    * ゲーム開始時の初期化 (PlayInitState)
    **/
   public static function initGame():Void {
-    _life = MAX_LIFE;
     _score = 0;
     _level = START_LEVEL;
-    _shot = FIRST_SHOT;
     _money = FIRST_MONEY;
     // プレイヤーパラメータ
     _param = new Params();
@@ -78,7 +66,6 @@ class Global {
    * レベル開始時の初期化 (PlayState)
    **/
   public static function initLevel():Void {
-    _keys = 0;
     _score = 0;
     _step = FloorInfoDB.getSteps(level);
 
@@ -92,33 +79,6 @@ class Global {
     }
   }
 
-  public static function getLifeRatio():Float {
-    return Math.floor(100 * _life / MAX_LIFE);
-  }
-  public static function isLifeDanger():Bool {
-    return getLifeRatio() < 40;
-  }
-  public static function setLife(v:Float):Void {
-    _life = v;
-    if(_life > MAX_LIFE) {
-      _life = MAX_LIFE;
-    }
-  }
-  public static function addLife(v:Float):Void {
-    setLife(_life + v);
-  }
-  public static function subLife(v:Float):Bool {
-    _life -= v;
-    if(_life < 0) {
-//      _life = 0;
-    }
-    if(_life <= 0) {
-      // 死亡
-      return true;
-    }
-
-    return false;
-  }
 
   public static function addScore(v:Int):Void {
     _score += v;
@@ -133,34 +93,6 @@ class Global {
   public static function setLevel(v:Int):Void {
     _level = v;
   }
-  public static function subShot(v:Float):Void {
-    _shot -= v;
-    if(_shot < 0) {
-      _shot = 0;
-    }
-  }
-  public static function addShot(v:Float):Void {
-    _shot += v;
-    if(_shot > MAX_SHOT) {
-      _shot = MAX_SHOT;
-    }
-  }
-  public static function getKey():Int {
-    return _keys;
-  }
-  public static function addKey():Void {
-    _keys++;
-  }
-  public static function subKey():Void {
-    _keys--;
-    if(_keys < 0) {
-      _keys = 0;
-    }
-  }
-  public static function hasKey():Bool {
-    return  _keys > 0;
-  }
-
   public static function addMoney(v:Int):Void {
     _money += v;
   }
@@ -176,10 +108,8 @@ class Global {
 
   // -----------------------------------------------
   // ■アクセサ
-  static function get_life()  { return _life;  }
   static function get_score() { return _score; }
   static function get_level() { return _level; }
-  static function get_shot()  { return _shot;  }
   static function get_money() { return _money; }
   static function get_step()  { return _step;  }
   static function get_shop()  { return _shop;  }
