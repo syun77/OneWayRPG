f2029b5395cbf72b099a1e19d623714b219e40a6
syun77 <2dgames.jp@gmail.com>
2016-05-20 21:44:35 +0900

    fixed #66 OneWayRPG-66 [Flash] アイテム説明文の基本攻撃の文字がぼやけている

--- source/jp_2dgames/game/item/ItemDetail.hx
+++ source/jp_2dgames/game/item/ItemDetail.hx
@@ -74,7 +74,8 @@ class ItemDetail {
   public static function build(data:ItemDetailData):String {
     var ret = "";
     var cntBR:Int = 0;
-    ret += '攻: ${data.power} \n';
+    var power = TextUtil.fillSpace(data.power, 2); // flash対応
+    ret += '攻: ${power} \n';
     if(data.resists != 1.0) {
       ret += '属性: x ${data.resists}\n';
     }
